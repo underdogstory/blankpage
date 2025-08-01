821888e132f784123f8afa43c16f06b1c6638ffa
Artis Bautra <bautra.artis@gmail.com>
2025-08-01 22:47:34 +0300

    Initial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Blankpa.ge</title>
+        <link rel="icon" type="image/x-icon" href="favicons/favicon.ico">
+        <meta charset="utf-8">
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+        <meta content="Default page" name="description">
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        
+        <script
+            defer
+            data-website-id="688cd6f27e584185b96262f6"
+            data-domain="blankpa.ge"
+            src="https://datafa.st/js/script.js">
+        </script>
+    </head>
+    <body>
+    </body>
+</html>
